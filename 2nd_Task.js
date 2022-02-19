cdf823fa855c10934319ab5219b6615837e20eb6
CEM OZDEMIR <57351242+CemmOzdemir@users.noreply.github.com>
2022-02-20 01:14:55 +0300

    Create 2nd_Task.js

---
+++ 2nd_Task.js
@@ -0,0 +1,15 @@
+
+                            //Reverse a String
+//Reverse the provided string.
+//You may need to turn the string into an array before you can reverse it.
+//Your result must be a string.
+
+function reverseString(str) {
+  let emptyStr = ""
+  for(let i =str.length-1 ; i>=0; i--){
+    emptyStr +=str[i];
+  }
+  return emptyStr;
+}
+
+reverseString("hello");
